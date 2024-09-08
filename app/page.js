380310191b442ae2c4e7af82f7c52aae43e5a8c4
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-09-08 13:03:10 +0530

    Refactor audio output device selection in Console component and update lossless audio UI

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className="h-svh max-h-svh w-screen relative overflow-hidden">
       {/* <img src="https://wallpaperswide.com/download/light_background-wallpaper-3000x2000.jpg" className="h-full w-full opacity-40 absolute inset-0" alt="" /> */}
-      <div className="absolute bg-neutral-100 inset-0 h-svh w-svh overflow-y-auto pt-2 md:pt-20">
+      <div className="absolute bg-neutral-100 inset-0 h-svh w-svh overflow-y-auto pt-2 md:pt-16">
         <div className="w-[97%] md:w-[950px] h-fit pb-10 bg-white border rounded-lg md:rounded-[30px] shadow-neutral-200 mx-auto relative overflow-hidden">
           <Console setIsAboutOpen={setIsAboutOpen} />
           {isAboutOpen && <About isAboutOpen={isAboutOpen} setIsAboutOpen={setIsAboutOpen} />}
