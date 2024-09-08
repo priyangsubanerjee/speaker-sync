e1dd44201a58b295666da74b0758ac9bc3999789
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-09-08 13:01:48 +0530

    Refactor audio output device selection in Console component and remove commented out FAQ section

--- app/page.js
+++ app/page.js
@@ -16,26 +16,7 @@ export default function Home() {
           <Console setIsAboutOpen={setIsAboutOpen} />
           {isAboutOpen && <About isAboutOpen={isAboutOpen} setIsAboutOpen={setIsAboutOpen} />}
         </div>
-        {/* <div className="w-[97%] md:w-[950px] mx-auto mt-20 pt-12">
-          <h2 className="text-xl text-center font-semibold">Frequently Asked Questions</h2>
-          <p className="text-center text-neutral-600 mt-2">Find answers to common questions about SpeakerSync.</p>
-          <div className="max-w-2xl mx-auto">
-            <Accordion selectionMode="multiple">
-              <AccordionItem key="1" aria-label="Accordion 1" title={<h2>How do I change the audio output device?</h2>}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dolores, distinctio ipsam voluptate fugit eos nemo illum consequuntur officiis id ducimus libero repellat dolor quam atque
-                fuga, consectetur quia iusto!
-              </AccordionItem>
-              <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dolores, distinctio ipsam voluptate fugit eos nemo illum consequuntur officiis id ducimus libero repellat dolor quam atque
-                fuga, consectetur quia iusto!
-              </AccordionItem>
-              <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dolores, distinctio ipsam voluptate fugit eos nemo illum consequuntur officiis id ducimus libero repellat dolor quam atque
-                fuga, consectetur quia iusto!
-              </AccordionItem>
-            </Accordion>
-          </div>
-        </div> */}
+        <div className="h-10"></div>
       </div>
     </main>
   );
